7f00e45fd5804f2ec46d5c26708c93fd315c7fe0
masaki-dot <y.masaki0526gmail.com>
2021-04-20 04:58:53 +0000

    update 管理者商品新規投稿画面

--- app/controllers/manager/products_controller.rb
+++ app/controllers/manager/products_controller.rb
@@ -1,2 +1,31 @@
 class Manager::ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    # redirect_to manager_product_path(@product.id)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:genre_id,:name,:introduction,:price,:image,:is_active)
+  end
 end

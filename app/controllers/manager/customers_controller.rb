24e186322da70d2fcc08bc03d6e36c1bfc3da053
EC2 Default User <ec2-user@ip-172-31-90-243.ec2.internal>
2021-04-21 01:46:02 +0000

    InitialCommit

--- app/controllers/manager/customers_controller.rb
+++ app/controllers/manager/customers_controller.rb
@@ -1,2 +1,11 @@
 class Manager::CustomersController < ApplicationController
+    def index
+        @customer = Customer.all
+    end
+    def show
+        @customer = Customer.find(params[:id])
+    end
+    def edit
+        @customer = Customer.find(params[:id])
+    end
 end

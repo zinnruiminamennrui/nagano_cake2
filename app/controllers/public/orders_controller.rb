66f2dbd1b577c24829948e27703ab1128f69cbd4
masaki-dot <y.masaki0526gmail.com>
2021-04-25 01:12:48 +0000

    update oder

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,22 +1,19 @@
 class Public::OrdersController < ApplicationController
 
   def new
-    @addressees = current_customer.addressees
     @order = Order.new
   end
 
   def confirmation
     @orders = current_customer.orders
-
   end
 
   def create
     @order = current_customer.orders.new(order_params)
     @order.save
-
-   
+    redirect_to orders_confirmation_path
   end
-
+  
   def complete
   end
 
@@ -26,10 +23,5 @@ class Public::OrdersController < ApplicationController
   def show
   end
 
-  private
-
-  def order_params
-    params.require(:order).permit(:postal_code, :address, :name, :payment_method, :total_payment,:postage,:status)
-  end
 
 end

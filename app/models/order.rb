70bfd0d9c7dd2b54710bcea319c7378107c37d81
hiromutomisawa <peachcocoaht202101@gmail.com>
2021-04-26 08:33:29 +0000

    [add]orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -13,4 +13,13 @@ class Order < ApplicationRecord
     preparing:3,
     delivered:4
   }
+
+  def total_quantity
+    sum = 0
+    order_products.each do |order_product|
+      sum += order_product.quantity
+    end
+    return sum
+  end
 end
+

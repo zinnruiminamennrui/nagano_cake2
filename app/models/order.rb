8f8db0870e1668dcf0274a0e4ee76441bb263923
hiromutomisawa <peachcocoaht202101@gmail.com>
2021-04-26 03:07:23 +0000

    [add]status

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,12 @@ class Order < ApplicationRecord
     credit_card:0,
     bank_transfer:1
   }
+
+  enum status: {
+    waiting:0,
+    cofirmartion:1,
+    in_prodouction:2,
+    preparing:3,
+    delivered:4
+  }
 end

69c65a1b576290aff303e0cf59d3c8901a63e262
masaki-dot <y.masaki0526gmail.com>
2021-04-26 08:39:33 +0000

    updateステータス系

--- app/controllers/manager/order_products_controller.rb
+++ app/controllers/manager/order_products_controller.rb
@@ -1,2 +1,13 @@
 class Manager::OrderProductsController < ApplicationController
+
+  def update
+    @order_product = OrderProduct.find(params[:id])
+    @order_product.update(order_product_params)
+    redirect_to manager_order_path(@order_product.order.id)
+  end
+
+  private
+	def order_product_params
+		  params.require(:order_product).permit(:making_status)
+	end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,10 +7,10 @@ class Order < ApplicationRecord
   }
 
   enum status: {
-    waiting:0,
-    cofirmartion:1,
-    in_prodouction:2,
-    preparing:3,
-    delivered:4
+    入金待ち:0,
+    入金確認:1,
+  　製作中:2,
+    発送準備中:3,
+    発送済み:4
   }
 end

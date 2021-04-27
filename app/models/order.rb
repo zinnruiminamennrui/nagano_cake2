487ae2346de36794a0849a6a1cb4392e06f54697
masaki-dot <y.masaki0526gmail.com>
2021-04-27 03:08:08 +0000

    update

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,7 @@ class Order < ApplicationRecord
   enum status: {
     入金待ち:0,
     入金確認:1,
-  　製作中:2,
+    製作中:2,
     発送準備中:3,
     発送済み:4
   }
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -5,7 +5,7 @@ class OrderProduct < ApplicationRecord
   enum making_status: {
     制作不可:0,
     制作待ち:1,
-  　製作中:2,
+    製作中:2,
     制作完了:3
   }
 end
